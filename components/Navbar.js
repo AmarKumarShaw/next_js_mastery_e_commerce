cb3bcefaa9d32ff534a6e68d652e62031e60a85c
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2022-10-23 20:53:53 +0530

    Cart Added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,48 +1,64 @@
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-import {AiFillCloseCircle, AiOutlineShoppingCart} from "react-icons/ai"
+import { AiFillCloseCircle, AiOutlineShoppingCart, AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai"
+import {BsFillBagCheckFill,BsBagXFill} from "react-icons/bs"
 import { useRef } from 'react';
 
 const Navbar = () => {
 
   const toggleCart = () => {
+    if (ref.current.classList.contains('translate-x-full')) {
+      ref.current.classList.remove('translate-x-full')
+      ref.current.classList.add('translate-x-0')
+    }
+    else if (!ref.current.classList.contains('translate-x-full')) {
+      ref.current.classList.remove('translate-x-0')
+      ref.current.classList.add('translate-x-full')
+    }
 
-    
   }
 
   const ref = useRef()
 
   return (
     <div className='flex flex-col md:flex-row md:justify-start justify-center items-center mb-1 py-2 shadow-md'>
-    <div className="logo mx-5">
-      <Link  href={"/"}>
-        <a href="">
-          <Image src="/images/thai.png" height={40} width={40} alt="Logo" />
-        </a>
-      </Link>
-    </div>
-    <div className="nav">
-    <ul className='flex items-center space-x-6 font-bold md:text-md '>
-      <Link href={"/tshirt"}><a><li>Tshirts</li></a></Link>
-      <Link href={"/hoodies"}><a><li>Hoodies</li></a></Link>
-      <Link href={"/stickers"}><a><li>Stickers</li></a></Link>
-      <Link href={"/mugs"}><a><li>Mugs</li></a></Link>
-    </ul>
-    </div>
-      <div className="cart absolute right-0 mx-5">
+      <div className="logo mx-5">
+        <Link href={"/"}>
+          <a href="">
+            <Image src="/images/thai.png" height={40} width={40} alt="Logo" />
+          </a>
+        </Link>
+      </div>
+      <div className="nav">
+        <ul className='flex items-center space-x-6 font-bold md:text-md '>
+          <Link href={"/tshirt"}><a><li>Tshirts</li></a></Link>
+          <Link href={"/hoodies"}><a><li>Hoodies</li></a></Link>
+          <Link href={"/stickers"}><a><li>Stickers</li></a></Link>
+          <Link href={"/mugs"}><a><li>Mugs</li></a></Link>
+        </ul>
+      </div>
+      <div onClick={toggleCart} className="cart absolute right-0 mx-5">
         <button><AiOutlineShoppingCart className="text-xl md:text-2xl" /></button>
       </div>
 
       {/* ===============Sidebar Start Here================== */}
-          <div ref={ref} className="sideCart sidebar absolute top-0 right-0 bg-pink-100 p-10 transform transition-transform translate-x-full">
-            <h2 className='font-bold text-xl' >Shopping Cart </h2>
-            <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle /></span>
-          <ol>
-            <li><span>Tshirt - Wear the Code</span></li>
-          </ol>
-
-          </div>
+      <div ref={ref} className="w-72 h-full sideCart absolute top-0 right-0 bg-pink-100 p-10 transform transition-transform translate-x-full">
+        <h2 className='font-bold text-xl text-center' >Shopping Cart </h2>
+        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle /></span>
+        <ol className='list-decimal font-semibold'>
+          <li className='py-5'>
+            <div className="item flex">
+              <div className='w-2/3 font-semibold' >Tshirt - Wear the Code</div>
+              <div className='w-1/3 font-semibold flex items-center justify-center mx-2 text-lg'><AiFillMinusCircle className="cursor-pointer text-pink-500" /><span className='mx-2'>2</span><AiFillPlusCircle className="cursor-pointer text-pink-500"/></div>
+            </div>
+          </li>
+        </ol>
+        <div className='flex flex-col'>
+          <button class="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-4  focus:outline-none hover:bg-pink-600 rounded text-md"><BsFillBagCheckFill className="m-1"/>CheckOut</button>
+          <button class="flex mx-auto mt-2 text-white bg-pink-500 border-0 py-2 px-4 focus:outline-none hover:bg-pink-600 rounded text-md"><BsBagXFill className="m-1"/>Clear Cart</button>
+        </div>
+      </div>
       {/* ===============Sidebar Ends Here================== */}
 
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,67 @@
 import '../styles/globals.css'
 import Navbar from './../components/Navbar';
 import Footer from './../components/Footer';
-
+import {useState,useEffect} from "react"
 
 
 function MyApp({ Component, pageProps }) {
+  const [cart,setCart] = useState({})
+  const[subTotal,setSubTotal] = useState()
+  
+  useEffect(() => {
+    console.log("Hey I am using useffect from _app.js")
+    
+    try{
+      if(localStorage.getItem("cart")){
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      }
+    }catch(error){
+        console.log(error)
+        localStorage.clear()
+    }
+  },[])
+
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", myCart)
+  }
+
+      // =======================ADD TO CART=======================
+  const addToCart = (itemCode,qty,name,price,size,variant) => {
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode] = cart[itemCode].qty + qty
+    }
+    else{
+      newCart[itemCode] = {qty:1,price,name,size,variant}
+    }
+    setCart(newCart)
+    saveCart(newCart)  
+  }
+     
+  // =======================CLEAR CART=======================
+
+  const clearCart = () => {
+    setCart({})
+    saveCart({})
+  }
+  // =======================REMOVE FROM CART=======================
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if(itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty
+    }
+    if(newCart[itemCode]["qty"] <= 0){
+      delete newCart(itemCode)
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  } 
+
+
   return (
     <>
-  <Navbar />
-  <Component {...pageProps} />
+  <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart}  clearCart={clearCart} subTotal={subTotal} />
+  <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
   <Footer />
   </>
   )
